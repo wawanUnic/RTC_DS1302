b84257553b8b612e9edbefe55db7151fb0f41f02
wawanUnic <92972782+wawanUnic@users.noreply.github.com>
2022-04-05 14:13:26 +0300

    Add files via upload

---
+++ get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+ 
+# Use the class for the DS1302 RTC chip.
+import RTC_DS1302
+import os
+ 
+# Create an instance of the RTC class.
+ThisRTC = RTC_DS1302.RTC_DS1302()
+ 
+# Functions to read from the RTC chip.
+print("RAM: " + ThisRTC.ReadRAM())
+ 
+DateTime = { "Year":0, "Month":0, "Day":0, "Hour":0, "Minute":0, "Second":0 }
+Data = ThisRTC.ReadDateTime(DateTime)
+ 
+DateSetResponse = os.popen("date -s \"" + Data + "\"").read()
+ 
+print "Time has been set to: " + DateSetResponse
+ 
+# Finish with the Raspberry Pi GPIO pins.
+ThisRTC.CloseGPIO()
